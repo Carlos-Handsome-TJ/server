12172300243f7afc6514819904507cd9b136f7ca
Carlos-Handsome-TJ <201821100479@smail.xtu.edu.cn>
2022-06-19 23:10:29 +0800

    fix:路由鉴定

--- routers/content.js
+++ routers/content.js
@@ -2,17 +2,15 @@ const Router = require('koa-router')
 const {
   findContent
 } = require('../controller/content')
+const {
+  auth,
+  checkUserExist
+} = require('./middelware')
 const module = require("module");
 
 const router = new Router({prefix: 'user'})
 
-router.get('/', findContent)
-
-
-
-
-
-
+router.get('/', auth, checkUserExist, findContent)
 
 
 module.export = router
